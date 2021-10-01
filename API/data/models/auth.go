ec2ea5fc4e5837cb8dfc2244f4bd5da83bcff5a2
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-01 18:04:33 -0300

    Making values required

--- API/data/models/auth.go
+++ API/data/models/auth.go
@@ -1,7 +1,7 @@
 package models
 
 type AuthAccess struct {
-	Email       string `json:"email"`
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
+	Email       string `json:"email" validate:"required,email"`
+	AccessToken string `json:"access_token" validate:"required"`
+	TokenType   string `json:"token_type" validate:"required"`
 }
