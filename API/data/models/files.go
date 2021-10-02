221d78eba2cd2bb7d2444d59c8fdc22c2e2d60ef
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-02 10:32:06 -0300

    Removed owner email

--- API/data/models/files.go
+++ API/data/models/files.go
@@ -3,6 +3,5 @@ package models
 type File struct {
 	Id int64 `json:"id"`
 	OwnerId int64 `json:"-" validate:"required"`
-	OwnerEmail string `json:"owner_email" validade:"required,email"`
 	FileName string `json:"file_name" validate:"required"`
 }
