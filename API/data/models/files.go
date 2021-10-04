d6b1a35e60ba0cd3bf64f21d1ef0d3e1e556bf18
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-04 10:15:30 -0300

    Moved

--- API/data/models/files.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type File struct {
-	Id       int64  `json:"id" validate:"required"`
-	OwnerId  int64  `json:"-" validate:"required"`
-	FileName string `json:"file_name" validate:"required"`
-}
