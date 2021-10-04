1d2417c3c3b9ae22d276e498fd24d517c1dcee4b
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-04 11:53:46 -0300

    Grouping posts

--- API/main.go
+++ API/main.go
@@ -3,32 +3,15 @@ package main
 import (
 	"faceclone-api/data"
 	Users_router "faceclone-api/router/users"
+	Posts_router "faceclone-api/router/posts"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	//jwtware "github.com/gofiber/jwt/v3"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
-
-/* NOT USING YET
-/* Private request to when a user is logged in, requires access token to enter
-func private(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"success": true,
-		"path":    "private",
-	})
-}
-
-func public(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"success": true,
-		"path":    "public",
-	})
-}
-NOT USING YET */
 
 func main() {
 	// Try a connection to the database
@@ -48,22 +31,17 @@ func main() {
 	store := data.CreateStore()
 
 	// User router
-	user_group := app.Group("/users")
-	user_group.Static("/avatar", "./media/avatar")
-	Users_router.UserAuthRouter(user_group, *store)
-	Users_router.UserChangesRouter(user_group, *store)
-	Users_router.UserGettersRouter(user_group)
+	users_group := app.Group("/users")
+	users_group.Static("/avatar", "./media/avatar")
+	Users_router.UserAuthRouter(users_group, *store)
+	Users_router.UserChangesRouter(users_group, *store)
+	Users_router.UserGettersRouter(users_group)
 
-	/* NOT USING YET
-	privateAPI := app.Group("/private")
-	privateAPI.Use(jwtware.New(jwtware.Config{
-		SigningKey: []byte("secret"),
-	}))
-	privateAPI.Get("/", private)
-
-	publicApp := app.Group("/public")
-	publicApp.Get("/", public)
-	NOT USING YET */
+	// Posts router
+	posts_group := app.Group("/posts")
+	posts_group.Static("/post/media", "./media/post_media")
+	Posts_router.PostsControlRouter(posts_group, *store)
+	Posts_router.PostsGettersRouter(posts_group)
 
 	// 404 Handler
 	app.Use(func(c *fiber.Ctx) error {
