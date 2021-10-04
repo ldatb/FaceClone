7b1cd4bbcd42399f3da711c148161a67f9368fca
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-04 10:16:07 -0300

    Init

---
+++ API/data/models/post.go
@@ -0,0 +1,8 @@
+package models
+
+type Post struct {
+	Id          int64  `json:"id" validate:"required,number"`
+	OwnerId     int64  `json:"-" validate:"required,number"`
+	MediaId     int64  `json:"media_id" validate:"number"`
+	Description string `json:"description" validate:"alphanumunicode"`
+}
