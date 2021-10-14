aa8c472791e2730101fa4741a1241754ac8fd35b
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-14 12:25:41 -0300

    Array checking and deleting

--- API/utils/users.go
+++ API/utils/users.go
@@ -2,36 +2,9 @@ package utils
 
 import (
 	"os"
-		
-	"faceclone-api/data"
-	"faceclone-api/data/models"
 
 	"github.com/joho/godotenv"
-	"xorm.io/xorm"
 )
-
-/* This function fetches an user's avatar */
-func GetAvatar(id int64) (bool, *models.UserAvatar, *xorm.Engine, error) {
-	// Connect to database
-	DBengine, err := data.CreateDBEngine()
-	if err != nil {
-		return false, nil, nil, err
-	}
-
-	// Get user avatar
-	userAvatarRequest := new(models.UserAvatar)
-	has, err := DBengine.Table("user_avatar").Where("owner_id = ?", id).Desc("id").Get(userAvatarRequest)
-	if err != nil {
-		return false, userAvatarRequest, DBengine, err
-	}
-
-	// User not found
-	if !has {
-		return false, userAvatarRequest, DBengine, nil
-	}
-
-	return true, userAvatarRequest, DBengine, nil
-}
 
 func CreateAvatarUrl(filename string) (string, error) {
 	// Load url from .env
@@ -44,4 +17,25 @@ func CreateAvatarUrl(filename string) (string, error) {
 	URL := os.Getenv("APP_URL") + "/users/avatar/" + filename
 
 	return URL, nil
+}
+
+func Find(array []string, item string) bool {
+	for _, i := range array {
+		if i == item {
+			return true
+		}
+	}
+	return false
+}
+
+func FindAndDelete(array []string, item string) []string {
+	index := 0
+	for _, i := range array {
+		if i != item {
+			array[index] = i
+			index++
+		}
+	}
+
+	return array[:index]
 }
