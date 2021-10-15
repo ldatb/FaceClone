efc242cd9c9c80ea61605b0fe1d9dc732ee39d82
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-15 17:08:38 -0300

    Make file

---
+++ API/router/chat/chatMedia.go
@@ -0,0 +1,10 @@
+package Chat_router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+)
+
+func ChatMediaRouter(app fiber.Router, store session.Store) {
+	//app.Post("/create-comment", create_comment(store))
+}
