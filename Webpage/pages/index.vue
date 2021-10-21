91420fe67fb7784ad40cd6784c3cdc432af3eac5
Lucas de Ataides <lucasatab@gmail.com>
2021-10-21 18:44:05 -0300

    First pages

--- Webpage/pages/index.vue
+++ Webpage/pages/index.vue
@@ -1,20 +1,3 @@
 <template>
-    <div>
-        <BaseButton class="a" text="Teste Botão" btnlink="true" link="seila"/>
-        <BaseInput placeholder="Base Input"/>
-        <ExpandableTextArea placeholder="Expandable Text Area"/>
-        <StatusBadge class="status-online"/>
-    </div>
+    <HomeTemplate />
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-})
-</script>
-
-<style lang='scss' scoped>
-.a {
-    min-width: 13rem;
-}
-</style>
