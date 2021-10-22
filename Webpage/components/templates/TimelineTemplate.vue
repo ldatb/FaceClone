009115bfba908ebf4fa5d2c8817e2b9ac0a4bdf3
Lucas de Ataides <lucasatab@gmail.com>
2021-10-22 10:14:45 -0300

    Homepage Timeline

---
+++ Webpage/components/templates/TimelineTemplate.vue
@@ -0,0 +1,20 @@
+<template>
+    <Container class="feed-container">
+        <NewPostForm />
+
+        <div class="posts-container">
+            <PostCard v-for="n in 3" :key="n" />
+        </div>
+    </Container>
+</template>
+
+<style lang="scss" scoped>
+.feed-container {
+    display: grid;
+    gap: 1.3rem;
+}
+.posts-container {
+    display: grid;
+    gap: 1.5rem;
+}
+</style>
