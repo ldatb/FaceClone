2b593ec7be799f3de914be740254a0c72ffabdcb
Lucas de Ataides <lucasatab@gmail.com>
2021-10-23 22:08:13 -0300

    Search base page (not working yet, only testing)

---
+++ Webpage/pages/search/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <b>
+            {{ $route.query.query }}
+        </b>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    layout: 'blank'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
