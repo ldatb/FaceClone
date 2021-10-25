f4a8f5a4c388a3d82820fc6cd6cae5d83b8e1422
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-25 18:03:35 -0300

    Containerized the page

--- Webpage/components/templates/ProfileTemplate.vue
+++ Webpage/components/templates/ProfileTemplate.vue
@@ -1,20 +1,8 @@
 <template>
     <div class="profile-template">
-        <div class="profile-info">
-            <div class="profile-cover">
-                <img src="@/assets/img/capa.jpg" class="cover-image" />
-            </div>
-            <div class="profile-avatar">
-                <img src="@/assets/img/profile-pic.jpg" class="avatar-image" />
-                <span class="profile-name">Lucas Barreto</span>
-            </div>
-        </div>
+        <Profile coverurl="capa.jpg" avatarurl="profile-pic.jpg" username="Teste user" />
 
-        <div class="profile-tabs">
-            <ProfileTab link="/profile" text="Timeline" number=300 class="active-profile-tab" />
-            <ProfileTab link="/profile/followers" text="Followers" number=1000 />
-            <ProfileTab link="/profile/following" text="Following" number=20 />
-        </div>
+        <ProfileTabs timeline=300 followers=400 following=500 activetimeline=true />
 
         <ProfileFeed class="profile-feed" />
     </div>
@@ -29,55 +17,6 @@ export default Vue.extend({})
 .profile-template {
     display: grid;
     padding-bottom: 4rem;
-}
-.profile-info {
-    display: grid;
-    grid-auto-flow: row;
-
-    .profile-cover {
-        height: 25rem;
-        width: 100%;
-
-        .cover-image {
-            height: 100%;
-            width: 100%;
-            object-fit: cover !important;
-        }
-    }
-
-    .profile-avatar {
-        position: absolute;
-        top: 21.5rem;
-        display: grid;
-        gap: 2.5rem;
-        grid-auto-flow: row;
-        justify-self: center;
-
-        .avatar-image {
-            width: 16rem;
-            border: 0.5rem solid color(dark);
-            border-radius: 50%;
-            justify-self: center;
-        }
-
-        .profile-name {
-            justify-self: center;
-            font-weight: 700;
-            font-size: 1.6rem;
-            color: color(white);
-        }
-    }
-}
-.profile-tabs {
-    display: grid;
-    grid-auto-flow: column;
-    justify-content: center;
-    gap: 4rem;
-    padding-top: 18rem;
-
-    .active-profile-tab {
-        border-bottom: 2px solid color(blue);
-    }
 }
 .profile-feed {
     padding-top: 4rem;
