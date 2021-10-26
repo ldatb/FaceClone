589650be2c46e07d752073611cac391d96f2f9d9
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-26 18:01:43 -0300

    Made header sticky

--- Webpage/layouts/default.vue
+++ Webpage/layouts/default.vue
@@ -3,8 +3,8 @@
     <clientOnly>
       <notifications position="bottom center" classes="notifications" :max="1" />
     </clientOnly>
-    <Header />
-    <Nuxt />
+    <Header class="sticky-header"/>
+    <Nuxt class="page-content"/>
   </div>
 </template>
 
@@ -26,5 +26,14 @@ body {
 }
 .default-layout {
   min-height: 100%;
+}
+.sticky-header {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 0;
+}
+.page-content {
+  z-index: -1;
 }
 </style>
