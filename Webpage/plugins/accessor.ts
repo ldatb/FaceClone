6e51e118a224558824e092baa05d55d72e1a5304
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-28 17:31:19 -0300

    Remove Auth instance

--- Webpage/plugins/accessor.ts
+++ Webpage/plugins/accessor.ts
@@ -1,10 +1,9 @@
 import { Plugin } from '@nuxt/types'
-import { initializeAxios, initializeCookies, initializeAuth } from '@/utils/nuxt-instance'
+import { initializeAxios, initializeCookies } from '@/utils/nuxt-instance'
 
 const accessor: Plugin = ({ app }) => {
   initializeAxios(app.$axios)
   initializeCookies(app.$cookies)
-  initializeAuth(app.$auth)
 }
 
 export default accessor
--- Webpage/utils/nuxt-instance.ts
+++ Webpage/utils/nuxt-instance.ts
@@ -1,11 +1,9 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios'
 import { NuxtCookies } from 'cookie-universal-nuxt'
-import { Auth } from '@nuxtjs/auth-next'
 
 /* eslint-disable import/no-mutable-exports */
 let $axios: NuxtAxiosInstance
 let $cookies: NuxtCookies
-let $auth: Auth
 
 export const initializeAxios = (axiosInstance: NuxtAxiosInstance) => {
   $axios = axiosInstance
@@ -15,9 +13,4 @@ export const initializeCookies = (cookiesInstance: NuxtCookies) => {
   $cookies = cookiesInstance
 }
 
-
-export const initializeAuth = (authInstance: Auth) => {
-  $auth = authInstance
-}
-
-export { $axios, $cookies, $auth }
+export { $axios, $cookies }
