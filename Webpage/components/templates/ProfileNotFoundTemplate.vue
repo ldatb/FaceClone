0a5a25723946fa08f563c9282af6ad4f80521d77
Lucas de Ataides Barreto <lucasdeataides@hotmail.com>
2021-10-28 17:33:47 -0300

    Created (not working)

---
+++ Webpage/components/templates/ProfileNotFoundTemplate.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="profile-template">
+        <Profile avatarurl="NOT FOUND" name="NOT FOUND" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        avatarurl: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        postsquantity: {
+            type: String,
+            required: true,
+        },
+        followers: {
+            type: String,
+            required: true,
+        },
+        following: {
+            type: String,
+            required: true,
+        },
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.profile-template {
+    display: grid;
+    padding-bottom: 4rem;
+}
+</style>
